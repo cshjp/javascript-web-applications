fd855feae9fa90ca16ec91be056f2e262d32bd24
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-05-30 12:12:27 +0100

    changed the html to use bundle.js instead of index, made the methods add and multiply and got them to display the solution of a simple maths problem in the web console

--- example-1/index.js
+++ example-1/index.js
@@ -1,3 +1,5 @@
+const add = require('./add');
+const multiply = require('./multiply');
 // console.log doesn't print anything on the page
 // it is not meant to be visible to the user, but for you
 // to help in debugging and getting visibility in your JS code.
@@ -5,3 +7,4 @@
 // on Mac (using Chrome), use Option+Command+J to open the console and see this message.
 
 console.log('Hello from the developer console!');
+console.log(multiply(4, add(2, 2)));

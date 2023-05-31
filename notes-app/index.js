4fe77ace77ad9fce1d236be3cdce9ff3bd954583
Charlie Parker <cshjp@MASTUDENT-Charlie-Parker-C02G4FQVQ6L4.local>
2023-05-31 11:51:44 +0100

    example-3 unzipped

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,6 +1,13 @@
 const NotesModel = require('./notesModel')
+const NotesView = require('./notesView')
 
 const model = new NotesModel();
+const view = new NotesView(model);
+
+model.addNote('Example Note');
+
+view.displayNotes();
 
 console.log(model.getNotes());
+
 console.log('The notes app is running!')
